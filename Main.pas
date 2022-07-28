e449de5365e244210fd1b0e538b1a3ac816192e4
millipedes <millipedes@earthlink.net>
2022-07-28 15:20:05 -0600

    Written Large Segments of Token Module

--- Main.pas
+++ Main.pas
@@ -1,17 +1,24 @@
 program Main;
 uses
+  Token in 'FunctionalCFG/Token/Token.pas',
+  TokenType in 'FunctionalCFG/Token/TokenType.pas',
+  TokenStack in 'FunctionalCFG/Token/TokenStack.pas',
   GraphScale in 'GraphingPackage/GraphPresets/GraphScale.pas',
   Canvas in 'GraphingPackage/GraphEncoding/Canvas.pas';
 
 var
   P : TCanvas;
   GS : TGraphScale;
+  TT : TTokenStack;
 
 begin
-  GS := TGraphScale.Create(-10, 9, -10, 9);
-  P := TCanvas.Create(1000, 1000);
-  GS.WriteGraphScaleToCanvas(P);
-  P.Dump;
-  P.Free;
-  GS.Free;
+  {GS := TGraphScale.Create(-10, 9, -10, 9);}
+  {P := TCanvas.Create(1000, 1000);}
+  {GS.WriteGraphScaleToCanvas(P);}
+  TT := TTokenStack.Create(TToken.Create(TokenSin, 'sin'));
+  Writeln(TokenTypeToString(TT.GetValue.GetTokType));
+  {P.Dump;}
+  TT.Free;
+  {P.Free;}
+  {GS.Free;}
 end.
